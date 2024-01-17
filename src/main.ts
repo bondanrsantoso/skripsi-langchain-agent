e068a7e7905c87be69dd184772eedd857e05d104
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2024-01-17 18:49:08 +0700

    Added dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
+import { ConsoleLogger } from '@nestjs/common';
+
+const logger = new ConsoleLogger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 6996;
+
+  await app.listen(port);
+  logger.log('Listening on port ' + port);
+  logger.log('http://localhost:' + port);
 }
 bootstrap();
